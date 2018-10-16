8f682b98246a7afb47c1480a91a481c6a0c54cd2
VicSz <vicsz>
2018-10-16 12:53:49 -0400

    Ensure PrePostEnabled is enabled.

--- src/main/java/com/example/pcfssoresourceexample/PcfSsoResourceExampleApplication.java
+++ src/main/java/com/example/pcfssoresourceexample/PcfSsoResourceExampleApplication.java
@@ -2,13 +2,16 @@ package com.example.pcfssoresourceexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class PcfSsoResourceExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PcfSsoResourceExampleApplication.class, args);
 	}
+
 }
