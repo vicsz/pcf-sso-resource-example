cd0388e2b3447cd2d58abc14d8bcaacb4bed013e
VicSz <vicsz>
2018-10-16 10:13:53 -0400

    Initial commit

---
+++ src/main/java/com/example/pcfssoresourceexample/PcfSsoResourceExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.pcfssoresourceexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcfSsoResourceExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcfSsoResourceExampleApplication.class, args);
+	}
+}
